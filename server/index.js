65b470799ca04ee11199f94e3e2170f67a998a1b
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-08-01 20:02:11 -0700

    reorganized file heirarchy:

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const mysql = require('mysql');
-const passport = require('passport');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const keys = require('./config/keys');
 require('./services/passport');
@@ -19,12 +18,36 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// mySQL Client Setup
+const mysql = require('mysql');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: keys.sqlkey,
-  database: 'server6'
+  database: 'server6',
+  port: 3306
 });
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+//connection.query('CREATE TABLE IF NOT EXISTS values (number INT');
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
 
 require('./routes/postRoutes')(app, connection);
 require('./routes/authRoutes')(app);
---
+++ worker/index.js
@@ -0,0 +1,15 @@
+const redis = require('redis');
+const keys = require('keys');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, 1);
+});
+
+sub.subscribe('insert');
