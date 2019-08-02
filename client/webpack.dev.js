ffb7b44f78aca2881f1bc16067f64566ed46261c
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-08-01 18:26:21 -0700

    fixed location of bundle.js in html file:

---
+++ client/webpack.dev.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+      '/auth': 'http://localhost:5000'
+    },
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+});
