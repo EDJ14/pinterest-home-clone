fe6f134846f2f72b1941397db2c1ae401f68a30c
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-08-18 12:53:59 -0700

    ug

--- server/config/keys.js
+++ server/config/keys.js
@@ -3,11 +3,9 @@ module.exports = {
   googleClientID:
     '698149871795-b9shua1ueeapnnpible4kugpvfhvi9mb.apps.googleusercontent.com',
   googleClientSecret: 'DUXt2pi_hMOIcQ2EeXklFTUY',
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
   mysqlUser: process.env.MYSQLUSER,
   mysqlHost: process.env.MYSQLHOST,
-  mysqlDatabase: process.env.MYSQLDATABASE,
+  //mysqlDatabase: process.env.MYSQLDATABASE,
   mysqlPassword: process.env.MYSQLPASSWORD,
   mysqlPort: process.env.MYSQLPORT
 };
