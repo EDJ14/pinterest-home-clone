1bb4f7f4cfdb81a043a0c776cbca1cc98a6834e6
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-09-14 12:35:16 -0700

    d

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,7 +1,36 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require('path');
 
-module.exports = merge(common, {
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
   mode: 'production',
-  devtool: false
-});
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(png|gif|cur|jpg)$/,
+        loader: 'url-loader',
+        query: { limit: 8192 }
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  }
+};
