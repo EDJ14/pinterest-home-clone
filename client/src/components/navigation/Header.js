e5431a9811b3a5a599fb47ea381c34422790b1e8
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-09-14 11:14:10 -0700

    google auth

--- client/src/components/navigation/Header.js
+++ client/src/components/navigation/Header.js
@@ -150,9 +150,7 @@ class Header extends Component {
         <ButtonsCont>
           <HeaderUserButton bold text="Home" />
           <HeaderUserButton text="Following" />
-          <a href="http://192.168.99.101.xip.io:3050/auth/google">
-            {this.renderAuth()}
-          </a>
+          <a href="/auth/google">{this.renderAuth()}</a>
           <IconContext.Provider value={{ color: 'gray', size: '3rem' }}>
             <div style={{ padding: '0 1.5rem' }}>
               <TiMessageTyping />
